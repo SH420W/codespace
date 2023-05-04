dbe55a037432eea6447da665173e6693379e0f33
SHADOW <66161961+SH420W@users.noreply.github.com>
2023-05-04 15:08:40 +0000

    Thu, May 4, 2023, 3:08 PM +00:00

--- calculator.c
+++ calculator.c
@@ -1,9 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int void main()
+int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
-    printf("%i\n", x + y)
+    int z = x + y;
+    printf("%i\n", z);
 }
